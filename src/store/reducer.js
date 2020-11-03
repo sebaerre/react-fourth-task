aa144306173f2c1e4186b0cf96c8a3baada842eb
Sebastian Herrera <sherrera@technisys.com>
2020-11-03 10:58:41 -0300

    fourth task

---
+++ src/store/reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if(action.type === 'SUBSTRACT'){
+        return{
+            counter: state.counter - action.payload
+        }
+    }
+
+    if(action.type === 'ADD'){
+        return{
+            counter: state.counter + action.payload
+        }
+    }
+
+    if(action.type === 'DECREMENT'){
+        return{
+            counter: state.counter - 1
+        }
+    }
+    return state;
+};
+
+export default reducer;
